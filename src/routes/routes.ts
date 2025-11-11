a5d6c44ef4454d45e79b3f1d04a4eec89c30dbc3
Saket <saket.patil5@gmail.com>
2025-11-11 09:56:32 +0530

    Additions tothe view job file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -20,6 +20,7 @@ router.post("/login", authenticationController.login);
 router.post("/job-item", jobController.createJobitem);
 router.post("/createJob", requireAuth, customerController.createJob);    
 router.get("/viewJob", requireAuth, customerController.viewJob)
+router.get("/job-item", jobController.listJobItems);
 // router.post("/welcome", (req, res) => {return res.status(200).send("Done")});
 
 
